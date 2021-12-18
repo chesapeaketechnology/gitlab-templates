18fbdc5c13291e0224fe0e5e1a7b60ce70aeef12
corrigan.johnson <cjohnson@ctic-inc.com>
2021-12-18 15:35:16 -0800

    [release-job-improvements] Add debug statements

--- lib/gitlab/ci/templates/references/security/validate_security_scan.py
+++ lib/gitlab/ci/templates/references/security/validate_security_scan.py
@@ -13,11 +13,12 @@ for vulnerability in data['vulnerabilities']:
 
         excludedPackages = []
         if len(sys.argv) > 2:
+            print ("Excluded packages: " + sys.argv[2])
             excludedPackages = sys.argv[2].replace(' ', '')
             excludedPackages = excludedPackages.split(",")
             # Verify that the the critical vulnerability isn't part of any excluded packages
             if not any(packageName in string for string in excludedPackages):
-                print(str("Vulnerability detected: " + packageName))
+                print(str("Non excluded vulnerability detected: " + packageName))
                 exit(1)
         else:
             print(str("Vulnerability detected: " + packageName))
