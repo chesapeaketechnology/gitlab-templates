bd2ececcab1c0834cb552e58663d89e71f3973eb
corrigan.johnson <cjohnson@ctic-inc.com>
2021-12-18 22:10:09 -0800

    [release-job-improvements] Added stage to secret detection

--- lib/gitlab/ci/templates/references/security/validate_security_scan.py
+++ lib/gitlab/ci/templates/references/security/validate_security_scan.py
@@ -5,6 +5,9 @@ vulnerability_report = open(sys.argv[1])
 
 data = json.load(vulnerability_report)
 
+if len(sys.argv) > 2:
+    print ("Excluded packages: " + sys.argv[2])
+
 # Iterating through the json
 for vulnerability in data['vulnerabilities']:
     if vulnerability['severity'] == 'Critical':
@@ -13,7 +16,6 @@ for vulnerability in data['vulnerabilities']:
 
         excludedPackages = []
         if len(sys.argv) > 2:
-            print ("Excluded packages: " + sys.argv[2])
             excludedPackages = sys.argv[2].replace(' ', '')
             excludedPackages = excludedPackages.split(",")
             # Verify that the the critical vulnerability isn't part of any excluded packages
