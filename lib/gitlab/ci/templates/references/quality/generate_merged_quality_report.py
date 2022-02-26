b8277283648ef8da9206d8d59afc24e3fd90e9b7
corrigan.johnson <cjohnson@ctic-inc.com>
2022-02-25 20:49:31 -0800

    [1.2] Update file path

--- lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
+++ lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
@@ -7,5 +7,5 @@ for f in glob.glob('**/code-climate-file.json', recursive=True):
     print(f)
     with open(f,) as infile:
         data.extend(json.load(infile))
-    with open(".quality/merged_code-climate-file.json",'w') as outfile:
+    with open(".quality/merged-code-climate-file.json",'w') as outfile:
         json.dump(data, outfile)
