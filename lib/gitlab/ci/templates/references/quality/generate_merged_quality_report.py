0b12e0b2df4105a24cd5bc6a59a473335cba3e6e
corrigan.johnson <cjohnson@ctic-inc.com>
2022-02-25 21:25:47 -0800

    [1.2] Update python scrip to handle the case when the quality directory doesn't exist

--- lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
+++ lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
@@ -1,11 +1,16 @@
 import glob
 import json
+import os
 
 data = []
+directory = ".quality/"
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 for f in glob.glob('**/code-climate-file.json', recursive=True):
     print(f)
     with open(f,) as infile:
         data.extend(json.load(infile))
-    with open(".quality/merged-code-climate-file.json",'w') as outfile:
+    with open(directory + "/merged-code-climate-file.json",'w') as outfile:
         json.dump(data, outfile)
