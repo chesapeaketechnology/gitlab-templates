456f078be633b056b29a75c781b8b5b39a3bd3b5
corrigan.johnson <cjohnson@ctic-inc.com>
2022-03-20 16:38:43 -0700

    [Issue-22-fix-broken-quality-report-links] Fix path substitution issue

--- lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
+++ lib/gitlab/ci/templates/references/quality/generate_merged_quality_report.py
@@ -18,7 +18,7 @@ for f in glob.glob('**/code-climate-file.json', recursive=True):
     for item in data:
         location = item.get("location")
         pathReplacement = location.get("path")
-        pathReplacement = re.sub(regex, "src", pathReplacement)
+        pathReplacement = re.sub(regex, "src/", pathReplacement)
         location["path"] = pathReplacement
     with open(directory + "/merged-code-climate-file.json",'w') as outfile:
         json.dump(data, outfile)
