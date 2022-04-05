d947560ff42425d0f641bbeeaf1b686cb7796982
eventurino <eventurino@ctic.us>
2022-04-05 16:55:44 -0600

    Adds quality page aggregation for multi-module proejcts for Gitlab Pages/Badges. Also small refactors for JavaDoc multi-module aggregation.

--- lib/gitlab/ci/templates/references/quality/create_aggregate_quality_page.py
+++ lib/gitlab/ci/templates/references/quality/create_aggregate_quality_page.py
@@ -6,22 +6,23 @@ module_to_index_directory = {}
 for root, dirs, files in os.walk("./"):
     for file in files:
         index_path = os.path.join(root, file)
-        if "/build/docs/javadoc/index.html" in index_path:
-             module = index_path.replace("./", "").replace("/build/docs/javadoc/index.html", "")
-             index_directory = index_path.replace("index.html", "")
+        if "/build/reports/pmd/main.html" in index_path:
+             module = index_path.replace("./", "").replace("/build/reports/pmd/main.html", "")
+             index_directory = index_path.replace("main.html", "")
              module_to_index_directory[module] = index_directory
 
-index_html_output = open("public/javadocs/index.html","w")
+index_html_output = open("public/quality/index.html","w")
 index_html_output.write("<html>\n")
 index_html_output.write("<head>\n")
-index_html_output.write("<title>JavaDoc Resources</title>\n")
+index_html_output.write("<title>Quality Resources</title>\n")
 index_html_output.write("</head>\n")
 index_html_output.write("<body>\n")
 
 for module, index_directory in module_to_index_directory.items():
-    destination_directory = "public/javadocs/"+module
+    print(module, index_directory)
+    destination_directory = "public/quality/"+module
     shutil.copytree(index_directory, destination_directory)
-    value_index = '<h1><a href="./' + module + '/index.html">' + module + '</a></h1>\n'
+    value_index = '<h1><a href="./' + module + '/main.html">' + module + '</a></h1>\n'
     index_html_output.write(value_index)
 
 index_html_output.write("</body>\n")
