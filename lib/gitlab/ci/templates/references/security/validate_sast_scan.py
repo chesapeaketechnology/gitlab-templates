9687c34387573db1afa1f4085e8f55c7c5dd221b
eventurino <eventurino@ctic.us>
2023-11-20 11:13:28 -0700

    Fixes gradle sast job which was calling the wrong python script. Also updated python script to list all sast vulnerabilities found instead of failing on the first vulnerability found.

--- lib/gitlab/ci/templates/references/security/validate_sast_scan.py
+++ lib/gitlab/ci/templates/references/security/validate_sast_scan.py
@@ -5,27 +5,31 @@ vulnerability_report = open(sys.argv[1])
 
 data = json.load(vulnerability_report)
 
+excluded_files = []
 if len(sys.argv) > 2:
-    print ("Excluded packages: " + sys.argv[2])
+    excluded_files_arg = sys.argv[2]
+    trimmed_string = excluded_files_arg.replace(' ', '')
+    excluded_files = trimmed_string.split(",")
+    print("Excluded files:")
+    print(excluded_files)
 
+vulnerabilities_found = set()
 # Iterating through the json
 for vulnerability in data['vulnerabilities']:
     if vulnerability['severity'] == 'Critical':
         location = vulnerability['location']
-        filePath = location['file']
-
-        excludedFilePaths = []
-        if len(sys.argv) > 2:
-            excludedFilePaths = sys.argv[2].replace(' ', '')
-            excludedFilePaths = excludedFilePaths.split(",")
-            # Verify that the the critical vulnerability isn't part of any excluded packages
-            if not any(filePath in string for string in excludedFilePaths):
-                print(str("Non excluded vulnerability detected: " + filePath))
-                exit(1)
-        else:
-            print(str("Vulnerability detected: " + filePath))
-            exit(1)
-
+        file_name = location['file']
+        # Verify that the critical vulnerability isn't part of any excluded file
+        if file_name not in excluded_files:
+            vulnerabilities_found.add(file_name)
 
 # Closing file
 vulnerability_report.close()
+
+if len(vulnerabilities_found) > 0:
+    print("Found " + str(len(vulnerabilities_found)) + " vulnerabilities.")
+    for vulnerability in vulnerabilities_found:
+        print(vulnerability)
+    exit(1)
+else:
+    print("No vulnerabilities found.")
